91ac0f32ce318b0c8b9c2ad3a8826fa25b69d2b1
momosang <59814840+momosang98@users.noreply.github.com>
2021-01-12 19:18:22 +0800

    Update shuileidata.py

--- Chapter 2/shuileidata.py
+++ Chapter 2/shuileidata.py
@@ -4,7 +4,9 @@ import pylab
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
+
 #2.1 read data from uci data repository
+
 target_url = ("https://archive.ics.uci.edu/ml/machine-learning-"
 "databases/undocumented/connectionist-bench/sonar/sonar.all-data")
 data = urllib.request.urlopen(target_url)
@@ -18,7 +20,9 @@ for line in data:
 sys.stdout.write("Number of Rows of Data = " + str(len(xList)) + '\n')
 sys.stdout.write("Number of Columns of Data = " + str(len(xList[1]))+ '\n')
 
+
 #2.2 check data_type
+
 nrow = len(xList)
 ncol = len(xList[1])
 type = [0]*3
@@ -44,10 +48,11 @@ for types in colCounts:
                      str(types[1]) + '\t\t' + str(types[2]) + "\n")
     iCol += 1
 
+    
 #2.3 Summary
+
 #arrange data into list for labels and list of lists for attributes
 labels = []
-
 #generate summary statistics for column 3 (e.g.)
 col = 3
 colData = []
@@ -58,26 +63,25 @@ colMean = np.mean(colArray)
 colsd = np.std(colArray)
 sys.stdout.write("Mean = " + '\t' + str(colMean) + '\t\t' +
                  "Standard Deviation = " + '\t ' + str(colsd) + "\n")
+
 #calculate quantile boundaries
 ntiles = 4
-
 percentBdry = []
-
 for i in range(ntiles+1):
     percentBdry.append(np.percentile(colArray, i * (100) / ntiles))
 sys.stdout.write("\nBoundaries for 4 Equal Percentiles \n")
 print(percentBdry)
 sys.stdout.write(" \n")
+
 #run again with 10 equal intervals
 ntiles = 10
-
 percentBdry = []
-
 for i in range(ntiles+1):
     percentBdry.append(np.percentile(colArray, i * (100) / ntiles))
 sys.stdout.write("Boundaries for 10 Equal Percentiles \n")
 print(percentBdry)
 sys.stdout.write(" \n")
+
 #The last column contains categorical variables
 col = 60
 colData = []
@@ -86,6 +90,7 @@ for row in xList:
 unique = set(colData)
 sys.stdout.write("Unique Label Values \n")
 print(unique)
+
 #count up the number of elements having each value
 catDict = dict(zip(list(unique),range(len(unique))))
 catCount = [0]*2
@@ -97,7 +102,9 @@ print(catCount)
 
 '''so, the last number 4 is outlier'''
 
+
 #2.4 qqplotAttribute 用分位数图展示异常点
+
 #generate summary statistics for column 3 (e.g.)
 col = 3
 colData = []
@@ -106,13 +113,14 @@ for row in xList:
 stats.probplot(colData, dist="norm", plot= pylab)
 pylab.show()
 
+
 #2.5 用Python Pandas 读入数据、分析数据
 target_url = ("https://archive.ics.uci.edu/ml/machine-learning-"
 "databases/undocumented/connectionist-bench/sonar/sonar.all-data")
 #read rocks versus mines data into pandas data frame
 rocksVMines = pd.read_csv(target_url,header=None)
 
-#print head and tail of data frame,5
+#print head and tail of data frame, 前5后5行
 print(rocksVMines.head())
 print(rocksVMines.tail())
 
